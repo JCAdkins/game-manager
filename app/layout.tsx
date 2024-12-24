eb7f1121be6afd697bf5436a2ea7ae67848acf4b
Jordan Adkins <jordan.adkins111@gmail.com>
2024-12-23 21:09:57 -0800

    Got rid of unused var.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Sidebar } from "@/components/sidebar";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const LIGHT_THEME_COLOR = "hsl(0 0% 100%)";
