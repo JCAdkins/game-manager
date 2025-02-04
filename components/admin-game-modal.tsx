5fd915d8df87cae8c69f4824d552e3b5c59177d5
Jordan Adkins <jordan.adkins111@gmail.com>
2025-02-04 03:51:11 -0800

    Changed params to a Promise<{}> object in route 'admin/edit/[gameId].

--- components/admin-game-modal.tsx
+++ components/admin-game-modal.tsx
@@ -1,17 +1,18 @@
 export const AdminGameModal = () => {
-  return;
-  <div className="flex w-full justify-evenly">
-    <button
-      className="px-4 py-1 rounded-lg bg-background/50 border-1 border-background hover:bg-background/70"
-      onClick={() => console.log("Editing Genre")}
-    >
-      Edit{" "}
-    </button>
-    <button
-      className="px-3 py-1 rounded-lg bg-background/50 border-1 border-background hover:bg-background/70"
-      onClick={() => setIsOpen(false)}
-    >
-      Close
-    </button>
-  </div>;
+  // return (
+  //   <div className="flex w-full justify-evenly">
+  //     <button
+  //       className="px-4 py-1 rounded-lg bg-background/50 border-1 border-background hover:bg-background/70"
+  //       onClick={() => console.log("Editing Genre")}
+  //     >
+  //       Edit{" "}
+  //     </button>
+  //     <button
+  //       className="px-3 py-1 rounded-lg bg-background/50 border-1 border-background hover:bg-background/70"
+  //       onClick={() => setIsOpen(false)}
+  //     >
+  //       Close
+  //     </button>
+  //   </div>
+  // );
 };
