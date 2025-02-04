d8413b192fbdcb31c9c3f3df58ca731bd52f4ef6
Jordan Adkins <jordan.adkins111@gmail.com>
2025-02-04 02:54:13 -0800

    Removed unused variables.

--- app/(admin)/admin/edit/[gameId]/page.tsx
+++ app/(admin)/admin/edit/[gameId]/page.tsx
@@ -6,9 +6,7 @@ export default async function AdminEditPage({
 }: {
   params: { gameId: string };
 }) {
-  /* Await is not currently needed for the app to work but it could change in the future.
-    Right now await is just suppressing a warning that Next.js gives about the future change. */
-  const { gameId } = await params;
+  const { gameId } = params;
 
   const game = await getGameById(gameId);
 
