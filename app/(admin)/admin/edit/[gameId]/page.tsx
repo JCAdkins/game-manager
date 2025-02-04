a2c22f6a64b22e44d65c7d9646113157abe43928
Jordan Adkins <jordan.adkins111@gmail.com>
2025-02-04 03:21:19 -0800

    Changed params to a Promise<{}> object in route 'admin/edit/[gameId].

--- app/(admin)/admin/edit/[gameId]/page.tsx
+++ app/(admin)/admin/edit/[gameId]/page.tsx
@@ -8,7 +8,7 @@ export default async function AdminEditPage({
 }) {
   /* Await is not currently needed for the app to work but it could change in the future.
     Right now await is just suppressing a warning that Next.js gives about the future change. */
-  const { gameId } = await params;
+  const { gameId }: { gameId: string } = await params;
   const game = await getGameById(gameId);
 
   if (!game) {
